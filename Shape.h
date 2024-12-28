d1f2c56199e171a494ce0140698c1977cf248294
mlmonesc <mmonerrise@gmail.com>
2024-12-28 19:36:39 +0100

    Añadida declaración de la clase Shape

---
+++ Shape.h
@@ -0,0 +1,24 @@
+#ifndef SHAPE_H
+#define SHAPE_H
+
+#include <string>
+#include "Point2D.h"
+using namespace std;
+
+class Shape{
+   protected:
+      string color;
+
+   public:
+      Shape();
+      Shape(string color);
+      string get_color() const;
+      void set_color(string c);
+      virtual double area() const = 0;
+      virtual double perimeter() const = 0;
+      virtual void translate(double incX, double incY) = 0; 
+      virtual void print()=0;
+      virtual double max_side() const = 0;
+
+};
+#endif
